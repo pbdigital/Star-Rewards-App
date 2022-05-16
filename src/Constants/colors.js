c2f0ebc43408d7ebbdbaa0fe95c8f890868d9ed5
Erny Marvin Dalire <ernymarvin.d@gmail.com>
2022-05-16 19:37:34 +0800

    feat: added brand colors

--- src/Constants/colors.js
+++ src/Constants/colors.js
@@ -1,3 +1,13 @@
 export const COLORS = {
   Black: '#000000',
+  Blue: '#80B5F0',
+  LightBlue: '#ABCFF4',
+  Yellow: '#F8D879',
+  LightYellow: '#FCEEBF',
+  Green: '#84CE7E',
+  LightGreen: '#C1ECBE',
+  Purple: '#8F75D7',
+  LightPurple: '#A995DF',
+  Red: '#E77872',
+  LightRed: '#EDA4A0',
 };
