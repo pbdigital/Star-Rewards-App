65a9a57c32ecbd2f4068690527362ee35857b675
Erny Marvin Dalire <ernymarvin.d@gmail.com>
2022-08-15 19:16:39 +0800

    feat: auto close on swipe on Rewards history list

--- src/Components/ListItems/RewardsHistoryListItem/index.js
+++ src/Components/ListItems/RewardsHistoryListItem/index.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {forwardRef, useCallback, useState} from 'react';
 import {ActivityIndicator, View} from 'react-native';
 import {COLORS} from 'Constants';
 import {Images} from 'Assets/Images';
@@ -12,153 +12,155 @@ import {ListSwipeControlButtons} from 'src/Components/ListSwipeControlButtons';
 import {ConfirmationModal} from 'Components';
 import Modal from 'react-native-modal';
 
-const RewardsHistoryListItem = ({
-  id,
-  name,
-  childId,
-  emoji,
-  starsNeededToUnlock,
-  date,
-  hideCloseButton,
-  marginTop,
-  marginBottom,
-  marginLeft,
-  marginRight,
-  isDeleting,
-}) => {
-  const dispatch = useDispatch();
-  const [
-    isDeleteConfirmationModalVisible,
-    setIsDeleteConfirmationModalVisible,
-  ] = useState(false);
-  const [showLoadingIndicator, setShowLoadingIndicator] = useState(false);
-
-  const renderItem = useCallback(() => {
-    return (
-      <Padded>
-        <Container
-          marginTop={marginTop}
-          marginBottom={marginBottom}
-          marginLeft={marginLeft}
-          marginRight={marginRight}>
-          <Details>
-            <Text fontSize={40} lineHeight={50} textAlign="center">
-              {emoji}
-            </Text>
-            <View style={{flex: 1, marginLeft: 16}}>
-              <Text
-                fontSize={18}
-                fontWeight="600"
-                lineHeight={27}
-                marginBottom={4}
-                color={COLORS.Text.black}>
-                {name}
-              </Text>
-              <Text
-                fontSize={14}
-                fontWeight="400"
-                lineHeight={21}
-                color={COLORS.Text.lightGrey}>
-                {moment(date).format('D/MM/YYYY')}
-              </Text>
-            </View>
-            <View>
-              <BonusStarInfo source={Images.Star}>
-                <Text fontSize={13} fontWeight="600" color="#B46C00">
-                  {starsNeededToUnlock}
-                </Text>
-              </BonusStarInfo>
-            </View>
-          </Details>
-          <ConfirmationModal
-            isVisible={isDeleteConfirmationModalVisible}
-            title="Are you sure you want to delete this reward?"
-            negativeButtonText="Cancel"
-            positiveButtonText="Delete"
-            buttonFontSize={20}
-            buttonTextColor={COLORS.Blue}
-            onPressPositiveButton={handleDeleteTask}
-            onClose={handleOnCloseConfirmationModal}
-            onPressNegativeButton={handleOnCloseConfirmationModal}
-          />
-          <Modal
-            isVisible={showLoadingIndicator}
-            animationIn={'fadeIn'}
-            animationOut={'fadeOut'}>
-            <ActivityIndicator />
-          </Modal>
-        </Container>
-      </Padded>
-    );
-  }, [
-    emoji,
-    starsNeededToUnlock,
-    name,
-    date,
-    marginTop,
-    marginBottom,
-    marginLeft,
-    marginRight,
-    showLoadingIndicator,
-    isDeleteConfirmationModalVisible,
-    handleDeleteTask,
-  ]);
-
-  const openDeleteConfirmationModal = () =>
-    setIsDeleteConfirmationModalVisible(true);
-
-  const handleOnCloseConfirmationModal = () =>
-    setIsDeleteConfirmationModalVisible(false);
-
-  const handleDeleteTask = useCallback(async () => {
-    setTimeout(() => setShowLoadingIndicator(true), 400);
-    handleOnCloseConfirmationModal();
-    const params = {
+const RewardsHistoryListItem = forwardRef(
+  (
+    {
+      id,
+      name,
       childId,
-      taskId: id,
-    };
-    const {payload} = await dispatch(
-      childActions.deleteCompletedTaskHistory(params),
-    );
-    if (payload?.success) {
-      await batch(() => {
-        dispatch(childActions.getRewardsHistory({childId}));
-        dispatch(
-          childActions.getChildTasks({childId, time: moment().format()}),
-        );
-      });
-    }
-    setTimeout(() => setShowLoadingIndicator(false), 500);
-  }, [childId, id]);
+      emoji,
+      starsNeededToUnlock,
+      date,
+      hideCloseButton,
+      marginTop,
+      marginBottom,
+      marginLeft,
+      marginRight,
+      isDeleting,
+      handleOnRowOpen,
+    },
+    ref,
+  ) => {
+    const dispatch = useDispatch();
+    const [
+      isDeleteConfirmationModalVisible,
+      setIsDeleteConfirmationModalVisible,
+    ] = useState(false);
+    const [showLoadingIndicator, setShowLoadingIndicator] = useState(false);
 
-  const renderHiddenItem = useCallback(() => {
+    const renderItem = useCallback(() => {
+      return (
+        <Padded>
+          <Container
+            marginTop={marginTop}
+            marginBottom={marginBottom}
+            marginLeft={marginLeft}
+            marginRight={marginRight}>
+            <Details>
+              <Text fontSize={40} lineHeight={50} textAlign="center">
+                {emoji}
+              </Text>
+              <View style={{flex: 1, marginLeft: 16}}>
+                <Text
+                  fontSize={18}
+                  fontWeight="600"
+                  lineHeight={27}
+                  marginBottom={4}
+                  color={COLORS.Text.black}>
+                  {name}
+                </Text>
+                <Text
+                  fontSize={14}
+                  fontWeight="400"
+                  lineHeight={21}
+                  color={COLORS.Text.lightGrey}>
+                  {moment(date).format('D/MM/YYYY')}
+                </Text>
+              </View>
+              <View>
+                <BonusStarInfo source={Images.Star}>
+                  <Text fontSize={13} fontWeight="600" color="#B46C00">
+                    {starsNeededToUnlock}
+                  </Text>
+                </BonusStarInfo>
+              </View>
+            </Details>
+            <ConfirmationModal
+              isVisible={isDeleteConfirmationModalVisible}
+              title="Are you sure you want to delete this reward?"
+              negativeButtonText="Cancel"
+              positiveButtonText="Delete"
+              buttonFontSize={20}
+              buttonTextColor={COLORS.Blue}
+              onPressPositiveButton={handleDeleteTask}
+              onClose={handleOnCloseConfirmationModal}
+              onPressNegativeButton={handleOnCloseConfirmationModal}
+            />
+            <Modal
+              isVisible={showLoadingIndicator}
+              animationIn={'fadeIn'}
+              animationOut={'fadeOut'}>
+              <ActivityIndicator />
+            </Modal>
+          </Container>
+        </Padded>
+      );
+    }, [
+      emoji,
+      starsNeededToUnlock,
+      name,
+      date,
+      marginTop,
+      marginBottom,
+      marginLeft,
+      marginRight,
+      showLoadingIndicator,
+      isDeleteConfirmationModalVisible,
+      handleDeleteTask,
+    ]);
+
+    const openDeleteConfirmationModal = () =>
+      setIsDeleteConfirmationModalVisible(true);
+
+    const handleOnCloseConfirmationModal = () =>
+      setIsDeleteConfirmationModalVisible(false);
+
+    const handleDeleteTask = useCallback(async () => {
+      setTimeout(() => setShowLoadingIndicator(true), 400);
+      handleOnCloseConfirmationModal();
+      const params = {
+        childId,
+        taskId: id,
+      };
+      const {payload} = await dispatch(
+        childActions.deleteCompletedTaskHistory(params),
+      );
+      if (payload?.success) {
+        await batch(() => {
+          dispatch(childActions.getRewardsHistory({childId}));
+          dispatch(
+            childActions.getChildTasks({childId, time: moment().format()}),
+          );
+        });
+      }
+      setTimeout(() => setShowLoadingIndicator(false), 500);
+    }, [childId, id]);
+
+    const renderHiddenItem = useCallback(() => {
+      return (
+        <Padded>
+          <ListSwipeControlButtons
+            key={`${id}-completed-task`}
+            item={{id}}
+            hideNeutralButton={true}
+            onPressDangerButton={openDeleteConfirmationModal}
+          />
+        </Padded>
+      );
+    }, [id]);
+
     return (
-      <Padded>
-        <ListSwipeControlButtons
-          key={`${id}-completed-task`}
-          item={{id}}
-          hideNeutralButton={true}
-          onPressDangerButton={openDeleteConfirmationModal}
-        />
-      </Padded>
+      <SwipeRow
+        ref={ref}
+        key={`${id}-rewards-tasks`}
+        rightOpenValue={-70}
+        leftOpenValue={0}
+        onRowOpen={handleOnRowOpen}>
+        {renderHiddenItem()}
+        {renderItem()}
+      </SwipeRow>
     );
-  }, [id]);
-
-  return (
-    <SwipeRow
-      // ref={ref => refTasksSwipeRow?.push(ref)}
-      key={`${id}-rewards-tasks`}
-      rightOpenValue={-70}
-      leftOpenValue={0}
-      // onRowPress={() => handleOnPressEditButton(item)}
-      onRowOpen={() => {
-        // closeRowExcept(refTasksSwipeRow, index);
-        // closeRowExcept(refBonusTasksSwipeRow, null);
-      }}>
-      {renderHiddenItem()}
-      {renderItem()}
-    </SwipeRow>
-  );
-};
+  },
+);
 
 export {RewardsHistoryListItem};
--- src/Components/RewardsHistory/index.js
+++ src/Components/RewardsHistory/index.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useEffect} from 'react';
+import React, {useMemo, useEffect, useState} from 'react';
 import {ScrollView, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {childActions, childIdSelector, rewardsHistorySelector} from 'Redux';
@@ -8,12 +8,24 @@ const RewardsHistory = () => {
   const dispatch = useDispatch();
   const childId = useSelector(childIdSelector);
   const rewardsHistory = useSelector(rewardsHistorySelector);
+  const [refTasksSwipeRow, setRefTasksSwipeRow] = useState([]);
 
   useEffect(() => {
     dispatch(childActions.getRewardsHistory({childId}));
   }, [childId]);
 
+  const closeRowExcept = (refSwipeTaskRow, activeIndex) => {
+    refSwipeTaskRow?.forEach((itemSwipeRow, index) => {
+      if (index === activeIndex) {
+        return;
+      }
+
+      itemSwipeRow?.closeRow();
+    });
+  };
+
   const renderCompleted = useMemo(() => {
+    setRefTasksSwipeRow([]);
     const extractedRewardHistories = Object.values(rewardsHistory).reduce(
       (prev, cur) => {
         return [...prev, ...cur];
@@ -23,10 +35,14 @@ const RewardsHistory = () => {
     return extractedRewardHistories.map((item, index) => {
       return (
         <RewardsHistoryListItem
+          ref={ref => refTasksSwipeRow?.push(ref)}
           {...item}
           hideCloseButton={true}
           marginTop={0}
           marginBottom={20}
+          handleOnRowOpen={() => {
+            closeRowExcept(refTasksSwipeRow, index);
+          }}
         />
       );
     });
