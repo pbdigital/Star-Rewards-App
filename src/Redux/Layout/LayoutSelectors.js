a00f0079a779a5f5bd44dd6389f44ae6d57ec178
ernymarvind <ernymarvin.d@gmail.com>
2024-02-14 05:59:12 +0800

    feat: default redux list type selector to stars

--- src/Redux/Layout/LayoutSelectors.js
+++ src/Redux/Layout/LayoutSelectors.js
@@ -1,9 +1,12 @@
+import {LIST_TYPE} from '../../Constants';
+
 export const toolbarStarPositionSelector = ({layout}) =>
   layout.toolbarStarPosition;
 
 export const toolBarStarAddedFlagSelector = ({layout}) =>
   layout.toolBarStarAddedFlag;
 
-export const starsViewListTypeSelector = ({layout}) => layout.starsView;
+export const starsViewListTypeSelector = ({layout}) =>
+  layout.starsView ?? LIST_TYPE.stars;
 export const bonusStarsViewListTypeSelector = ({layout}) =>
-  layout.bonusStarsView;
+  layout.bonusStarsView ?? LIST_TYPE.stars;
