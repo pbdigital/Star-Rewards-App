869006d7c1394a68a535947cdf981a7b097746d5
ernymarvind <ernymarvin.d@gmail.com>
2024-03-20 14:48:43 +0800

    chore: move styles on a separate file

--- src/Screens/Welcome/index.js
+++ src/Screens/Welcome/index.js
@@ -1,26 +1,32 @@
-import React, { useCallback, useEffect, useMemo, useRef } from 'react';
-import {Dimensions, TouchableOpacity, View} from 'react-native';
-import {Button, CloudImage, CustomBottomSheetBackdrop, Image, ScreenBackground, Text} from 'Components';
-import { Images } from 'src/Assets/Images';
-import { COLORS, NAV_ROUTES } from 'Constants';
+import React, {useEffect, useMemo, useRef} from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import {Button, CloudImage, Image, Text} from 'Components';
+import {Images} from 'src/Assets/Images';
+import {COLORS, NAV_ROUTES} from 'Constants';
 import LottieView from 'lottie-react-native';
-import { LottieAnimations } from 'src/Assets/LottieAnimations';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { CloudBackground1 } from 'src/Components/ScreenBackground/CloudBackgrounds';
-import { CloudBackground2 } from 'src/Components/ScreenBackground/CloudBackgrounds/CloudBackground2';
-import { CloudBackground3 } from 'src/Components/ScreenBackground/CloudBackgrounds/CloudBackground3';
-import { BottomSheetModal } from '@gorhom/bottom-sheet';
-import { ButtonContainer, FooterContainer } from './styles';
-import { doHapticFeedback } from 'Helpers';
-import { CommonActions, useNavigation } from '@react-navigation/native';
+import {LottieAnimations} from 'src/Assets/LottieAnimations';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {BottomSheetModal} from '@gorhom/bottom-sheet';
+import {
+  AnimationContainer,
+  AnimationWrapper,
+  ButtonContainer,
+  FooterContainer,
+  Monster1,
+  Monster2,
+  Monster3,
+  Root,
+  StarLogoContainer,
+  WelcomeNoteContainer,
+  styles,
+} from './styles';
+import {doHapticFeedback} from 'Helpers';
+import {CommonActions, useNavigation} from '@react-navigation/native';
 
 const WelcomeScreen = () => {
   const navigation = useNavigation();
   const bottomSheetModalRef = useRef(null);
   const snapPoints = useMemo(() => ['25%'], []);
-  const handleSheetChanges = useCallback(index => {
-    console.log('handleSheetChanges', index);
-  }, []);
 
   useEffect(() => {
     setTimeout(() => {
@@ -95,22 +101,9 @@ const WelcomeScreen = () => {
   };
 
   return (
-    <View style={{flex: 1, backgroundColor: COLORS.Background.screen}}>
+    <Root>
       <SafeAreaView edges={['top']} />
-      {/* <CloudImage style={{
-        // position: 'absolute',
-        // top: Dimensions.get('window').height * 0.30,
-        // left: -12
-        marginLeft: -12
-      }} /> */}
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          marginLeft: 37,
-          marginTop: 43,
-        }}
-      >
+      <StarLogoContainer>
         <Image source={Images.Logo} width={45} height={45} />
         <Text
           fontSize={17}
@@ -120,8 +113,8 @@ const WelcomeScreen = () => {
           color={COLORS.Text.black}>
           Star Rewards
         </Text>
-      </View>
-      <View style={{marginTop: 51}}>
+      </StarLogoContainer>
+      <WelcomeNoteContainer>
         <Text
           fontSize={36}
           fontWeight="600"
@@ -140,105 +133,44 @@ const WelcomeScreen = () => {
           color={COLORS.Text.grey}>
           Let's earn stars, unlock{'\n'}treasures, create memories!
         </Text>
-        {/* Bottom Left Cloud */}
-        <CloudImage style={{
-          position: 'absolute',
-          bottom: -50,
-          left: -12
-        }} width={79} height={52} />
-        {/* Upper Right Cloud */}
-        <CloudImage style={{
-          position: 'absolute',
-          top: -50,
-          right: -10,
-        }} width={94} height={62} />
-      </View>
-      <View style={{flex: 1}}>
-        <View style={{
-            backgroundColor: COLORS.LightBlue,
-            position: 'absolute',
-            flex: 1,
-            // height: 270,
-            height: Dimensions.get('window').height * 0.31,
-            width: '100%',
-            bottom: 0,
-          }} />
-        <View
-          style={{
-            width: '100%',
-            height: 300,
-            // backgroundColor: 'white',
-            position: 'relative',
-          }}>
-          <View
-            style={{
-              width: 300,
-              height: 300,
-              // backgroundColor: 'green',
-              position: 'absolute',
-              bottom: 0,
-              left: -60,
-            }}>
+        <CloudImage style={styles.bottomLeftCloud} width={79} height={52} />
+        <CloudImage style={styles.upperRightCloud} width={94} height={62} />
+      </WelcomeNoteContainer>
+      <AnimationContainer>
+        <View style={styles.animationBackdrop} />
+        <AnimationWrapper>
+          <Monster1>
             <LottieView
-              style={{
-                // backgroundColor: 'red',
-                width: 300,
-                height: 300,
-              }}
+              style={styles.monster1}
               source={LottieAnimations.Monster1SmallV1}
               autoPlay
               loop
             />
-          </View>
-          <View
-            style={{
-              width: 260,
-              height: 260,
-              // backgroundColor: 'green',
-              position: 'absolute',
-              bottom: 6,
-              right: -10,
-            }}>
+          </Monster1>
+          <Monster2>
             <LottieView
-              style={{
-                // backgroundColor: 'red',
-                width: 260,
-                height: 260
-              }}
+              style={styles.monster2}
               source={LottieAnimations.Monster2SmallV1}
               autoPlay
               loop
             />
-          </View>
-          <View
-            style={{
-              width: 200,
-              height: 200,
-              // backgroundColor: 'green',
-              position: 'absolute',
-              bottom: -16,
-              left: (Dimensions.get('window').width / 2) - 110,
-            }}>
+          </Monster2>
+          <Monster3>
             <LottieView
-              style={{
-                // backgroundColor: 'red',
-                width: 200,
-                height: 200
-              }}
+              style={styles.monster3}
               source={LottieAnimations.Monster3SmallV1}
               autoPlay
               loop
             />
-          </View>
-        </View>
-      </View>
+          </Monster3>
+        </AnimationWrapper>
+      </AnimationContainer>
       <BottomSheetModal
         ref={bottomSheetModalRef}
         index={0}
         snapPoints={snapPoints}
         handleComponent={null}
-        enableContentPanningGesture={false}
-        onChange={handleSheetChanges}>
+        enableContentPanningGesture={false}>
         <ButtonContainer>
           <Button
             borderRadius={16}
@@ -256,7 +188,7 @@ const WelcomeScreen = () => {
         edges={['bottom']}
         style={{backgroundColor: COLORS.LightBlue}}
       />
-    </View>
+    </Root>
   );
 };
 
--- src/Screens/Welcome/styles.js
+++ src/Screens/Welcome/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import {COLORS} from 'Constants';
+import {Dimensions, StyleSheet} from 'react-native';
 
 export const Button = styled.TouchableOpacity`
   padding-vertical: 15px;
@@ -22,3 +23,86 @@ export const FooterContainer = styled.View`
   align-items: center;
   flex-direction: row;
 `;
+
+export const Root = styled.View`
+  flex: 1;
+  background-color: ${COLORS.Background.screen};
+`;
+
+export const WelcomeNoteContainer = styled.View`
+  margin-top: 51px;
+`;
+
+export const StarLogoContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-left: 37px;
+  margin-top: 43px;
+`;
+
+export const AnimationContainer = styled.View`
+  flex: 1;
+`;
+
+export const AnimationWrapper = styled.View`
+  width: 100%;
+  height: 300px;
+  position: relative;
+`;
+
+export const Monster1 = styled.View`
+  width: 300px;
+  height: 300px;
+  position: absolute;
+  bottom: 0px;
+  left: -60px;
+`;
+
+export const Monster2 = styled.View`
+  width: 260px;
+  height: 260px;
+  position: absolute;
+  bottom: 6px;
+  right: -10px;
+`;
+
+export const Monster3 = styled.View`
+  width: 200px;
+  height: 200px;
+  position: absolute;
+  bottom: -16px;
+  left: ${(Dimensions.get('window').width / 2) - 110}px;
+`;
+
+export const styles = StyleSheet.create({
+  bottomLeftCloud: {
+    position: 'absolute',
+    bottom: -50,
+    left: -12,
+  },
+  upperRightCloud: {
+    position: 'absolute',
+    top: -50,
+    right: -10,
+  },
+  animationBackdrop: {
+    backgroundColor: COLORS.LightBlue,
+    position: 'absolute',
+    flex: 1,
+    height: Dimensions.get('window').height * 0.31,
+    width: '100%',
+    bottom: 0,
+  },
+  monster1: {
+    width: 300,
+    height: 300,
+  },
+  monster2: {
+    width: 260,
+    height: 260,
+  },
+  monster3: {
+    width: 200,
+    height: 200,
+  },
+});
